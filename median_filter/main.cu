81e78e9baab7de0cb79befd1e45ce68709c1dba8
Issam Said <isaid@nvidia.com>
2019-02-01 15:08:27 -0800

    update main.cu

--- median_filter/main.cu
+++ median_filter/main.cu
@@ -5,9 +5,9 @@
 #include <cuda_runtime.h>
 
 // program parameters:
-#define     N  128 // the cube size is N^3 
-#define     F  4   // the filter size id F^3
-#define     W  9
+#define     N  50  // the cube size is N^3. 
+#define     F  4   // the filter radius.
+#define     W  9   // the window size.
 #define DTYPE  float
 
 // CUDA helpers:
@@ -26,7 +26,11 @@ int median_filter_check(DTYPE *gpu_tab,
 	                      DTYPE *cpu_tab, DTYPE *tmp, int size) {
 	CUCHK(cudaMemcpy(tmp, gpu_tab, sizeof(DTYPE)*size, cudaMemcpyDeviceToHost));
 	int errors = 0;
-	for (int i=0; i<size; i++) if (fabs(tmp[i]-cpu_tab[i])>1.0e-4) errors++;
+	for (int i=0; i<size; i++)
+		if (fabs(tmp[i]-cpu_tab[i])>1.0e-4) {
+			//fprintf(stderr, "[%4d] g: %f c: %f\n", i, tmp[i], cpu_tab[i]);
+			errors++;
+		}
 	return errors;
 } 
 
@@ -59,7 +63,7 @@ int median_filter_cpu(DTYPE *tab, DTYPE *res, int ldim) {
 #define BLOCKX 8
 
 // the naive algorithm using global memory:
-__global__ void median_filter_gpu(DTYPE *gtab, DTYPE *gres, int ldim) {
+__global__ void median_filter_gpu_gm(DTYPE *gtab, DTYPE *gres, int ldim) {
 	int ix = blockIdx.x * BLOCKX + threadIdx.x + F;
 	int iy = blockIdx.y * BLOCKY + threadIdx.y + F;
 
@@ -77,42 +81,52 @@ __global__ void median_filter_gpu(DTYPE *gtab, DTYPE *gres, int ldim) {
 				}
 			}
 			// save the result on the output tab:
-			gres[idx] = v/((2*F+1)*(2*F+1)*(2*F+1));
+			gres[idx] = v/(W*W*W);
 			idx += pln;
 		}
 	}
 }
 
-__device__ void sm_read_plane(DTYPE shm[BLOCKY+2*F][BLOCKX+2*F], DTYPE *gtab, 
+__device__ void sm_read_plane(DTYPE shm[BLOCKY+2*F][BLOCKX+2*F],
+	                            DTYPE *gtab, 
 													    int ldim,
 													    int sx, int sy, 
 	                            int ix, int iy, int iz) {
-	shm[sy][sx] = gtab[(ix)+ldim*(ldim*(iz)+(iy))];
-	// +
-	if ((sy >= BLOCKY) && (iy<N+F)) {
-		shm[sy+F][sx] = gtab[(ix)+ldim*(ldim*(iz)+(iy+F))];
+	/*
+	for (int ly=-F; ly<=F; ly++) {
+		for (int lx=-F; lx<=F; lx++) {
+			shm[ly+sy][lx+sx] = gtab[(ix+lx)+ldim*(ldim*(iz)+(iy+ly))];
+		}
 	}
-	if ((sx >= BLOCKX) && (ix<N+F)) {
-		shm[sy][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz)+(iy))];
+	*/
+
+	
+	if ((sy < 2*F) && (sx < 2*F)) {
+		shm[sy-F][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz)+(iy-F))];
 	}
-	if ((sy >= BLOCKY) && (sx >= BLOCKX) && (iy<N+F) && (ix<N+F)) {
-		shm[sy+F][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz)+(iy+F))];			
-	}
-	if ((sy >= BLOCKY) && (sx < 2*F) && (iy<N+F) && (ix<N+F)) {
-		shm[sy+F][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz)+(iy+F))];			
-	}
-	// -
 	if (sy < 2*F) {
 		shm[sy-F][sx] = gtab[(ix)+ldim*(ldim*(iz)+(iy-F))];
+	}
+	if ((sy < 2*F) && (sx >= BLOCKX)) {
+		shm[sy-F][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz)+(iy-F))];
 	}
 	if (sx < 2*F) {
 		shm[sy][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz)+(iy))];
 	}
-	if ((sx < 2*F) && (sy < 2*F)) {
-		shm[sy-F][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz)+(iy-F))];
+	
+	shm[sy][sx] = gtab[(ix)+ldim*(ldim*(iz)+(iy))];
+	
+	if (sx >= BLOCKX) {
+		shm[sy][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz)+(iy))];
 	}
-	if ((sx >= BLOCKX) && (sy < 2*F)) {
-		shm[sy-F][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz)+(iy-F))];
+	if ((sy >= BLOCKY) && (sx < 2*F)) {
+		shm[sy+F][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz)+(iy+F))];			
+	}
+	if (sy >= BLOCKY) {
+		shm[sy+F][sx] = gtab[(ix)+ldim*(ldim*(iz)+(iy+F))];
+	}
+	if ((sy >= BLOCKY) && (sx >= BLOCKX)) {
+		shm[sy+F][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz)+(iy+F))];			
 	}
 	__syncthreads();
 }
@@ -123,12 +137,13 @@ __device__ void rg_copy_plane(DTYPE rg[W][W][W],
 	for (int ly=-F; ly<=F; ly++) {
 		for (int lx=-F; lx<=F; lx++) {
 			rg[rz][ly+F][lx+F] = shm[sy+ly][sx+lx];
+			//rg[rz][ly+F][lx+F] = shm[ly+F][lx+F];
 		}
 	}
 }
 
 __device__ void rg_shift_plane(DTYPE rg[W][W][W]) {
-	for (int lz=0; lz<W-1; lz++) {
+	for (int lz=0; lz<(W-1); lz++) {
 		for (int ly=0; ly<W; ly++) {
 			for (int lx=0; lx<W; lx++) {
 				rg[lz][ly][lx] = rg[lz+1][ly][lx];
@@ -137,9 +152,9 @@ __device__ void rg_shift_plane(DTYPE rg[W][W][W]) {
 	}
 }
 
-// this version uses shared memory and registers:
+// this version uses 2D shared memory and registers:
 __global__ void median_filter_gpu_sm_2d(DTYPE *gtab, DTYPE *gres, int ldim) {
-	__shared__ DTYPE shm[BLOCKY+2*F][BLOCKX+2*F];
+	__shared__ DTYPE shm[BLOCKY+W-1][BLOCKX+W-1];
 	int sx = threadIdx.x+F;
 	int sy = threadIdx.y+F;
 
@@ -150,16 +165,16 @@ __global__ void median_filter_gpu_sm_2d(DTYPE *gtab, DTYPE *gres, int ldim) {
 	off_t idx = (off_t)(F*pln)+ldim*iy+ix; 
 
 	DTYPE rg[W][W][W];
-	// bootstrap the registers:
-	for (int rz=0; rz<(2*F); rz++) {
-		sm_read_plane(shm, gtab, ldim, sx, sy, ix, iy, rz);
-		rg_copy_plane(rg, shm, sx, sy, rz);
-	}
-
-	for (int iz=F; iz<N+F; iz++) {
-		//if((ix<(N+F)) && (iy<(N+F)) && (iz<(N+F))) {
+	if((ix<(N+F)) && (iy<(N+F))) {
+		// bootstrap the registers:
+		for (int rz=0; rz<(W-1); rz++) {
+			sm_read_plane(shm, gtab, ldim, sx, sy, ix, iy, rz);
+			rg_copy_plane(rg, shm, sx, sy, rz);
+		}
+	
+		for (int iz=F; iz<N+F; iz++) {
 			sm_read_plane(shm, gtab, ldim, sx, sy, ix, iy, iz+F);
-			rg_copy_plane(rg, shm, sx, sy, 2*F);
+			rg_copy_plane(rg, shm, sx, sy, (W-1));
 			DTYPE v = 0;
 			for (int lz=0; lz<W; lz++) {	
 				for (int ly=0; ly<W; ly++) {
@@ -169,155 +184,9 @@ __global__ void median_filter_gpu_sm_2d(DTYPE *gtab, DTYPE *gres, int ldim) {
 				}
 			}
 			rg_shift_plane(rg);
-			/*
-			// apply the filter on each point, average (2*F+1)^3:
-			for (int lz=0; lz<F; lz++) {
-				for (int ly=0; ly<2*F+1; ly++) {
-					for (int lx=0; lx<2*F+1; lx++) {
-						//v += shm[threadIdx.z+lz][threadIdx.y+ly][threadIdx.x+lx];
-						v += gtab[(ix-F+lx)+ldim*(ldim*(iz-F+lz)+(iy-F+ly))];
-					}
-				}
-			}
-
-				for (int ly=-F; ly<=F; ly++) {
-					for (int lx=-F; lx<=F; lx++) {
-						v += shm[sy+ly][sx+lx];
-					}
-				}
-		
-			for (int lz=F+1; lz<2*F+1; lz++) {
-				for (int ly=0; ly<2*F+1; ly++) {
-					for (int lx=0; lx<2*F+1; lx++) {
-						//v += shm[threadIdx.z+lz][threadIdx.y+ly][threadIdx.x+lx];
-						v += gtab[(ix-F+lx)+ldim*(ldim*(iz-F+lz)+(iy-F+ly))];
-					}
-				}
-			}
-			*/
 			// save the result on the output tab:
-			gres[idx] = v/((2*F+1)*(2*F+1)*(2*F+1));
+			gres[idx] = v/(W*W*W);
 			idx      += pln;
-		//}
-	}
-}
-
-// this version uses shared memory:
-__global__ void median_filter_gpu_sm_3d(DTYPE *gtab, DTYPE *gres, int ldim) {
-	__shared__ DTYPE shm[BLOCKZ+2*F][BLOCKY+2*F][BLOCKX+2*F];
-	int sx = threadIdx.x + F;
-	int sy = threadIdx.y + F;
-	int sz = threadIdx.z + F;
-
-	int ix = blockIdx.x * BLOCKX + sx;
-	int iy = blockIdx.y * BLOCKY + sy;
-	
-	off_t pln = (off_t)ldim*ldim;
-	off_t idx = (off_t)(sz*pln)+ldim*iy+ix; 
-	
-	for (int iz=threadIdx.z+F; iz<N+F; iz+=BLOCKZ) {
-		
-		if((ix<(N+F)) && (iy<(N+F)) && (iz<(N+F))) {
-			// the main entry:
-			shm[sz][sy][sx] = gtab[(ix)+ldim*(ldim*(iz)+(iy))];
-			// front:
-			if ((sz < 2*F) && (sy < 2*F) && (sx < 2*F)) {
-				shm[sz-F][sy-F][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz-F)+(iy-F))];
-			}
-			if ((sz < 2*F) && (sy < 2*F)) {
-				shm[sz-F][sy-F][sx] = gtab[(ix)+ldim*(ldim*(iz-F)+(iy-F))];
-			}
-			if ((sz < 2*F) && (sy < 2*F) && (sx >= BLOCKX)) {
-				shm[sz-F][sy-F][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz-F)+(iy-F))];
-			}
-			if ((sz < 2*F) && (sx < 2*F)) {
-				shm[sz-F][sy][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz-F)+(iy))];
-			}
-			if (sz < 2*F) {
-				shm[sz-F][sy][sx] = gtab[(ix)+ldim*(ldim*(iz-F)+(iy))];
-			}
-			if ((sz < 2*F) && (sx >= BLOCKX)) {
-				shm[sz-F][sy][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz-F)+(iy))];			
-			}
-      if ((sz < 2*F) && (sy >= BLOCKY) && (sx < 2*F)) {
-				shm[sz-F][sy+F][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz-F)+(iy+F))];
-			}			
-			if ((sz < 2*F) && (sy >= BLOCKY)) {
-				shm[sz-F][sy+F][sx] = gtab[(ix)+ldim*(ldim*(iz-F)+(iy+F))];			
-			}
-		  if ((sz < 2*F) && (sy >= BLOCKY) && (sx >= BLOCKX)) {
-				shm[sz-F][sy+F][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz-F)+(iy+F))];
-			}
-			// back:
-			if ((sz >= BLOCKZ) && (sy < 2*F) && (sx < 2*F)) {
-				shm[sz+F][sy-F][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz+F)+(iy-F))];			
-			}
-			if ((sz >= BLOCKZ) && (sy < 2*F)) {
-				shm[sz+F][sy-F][sx] = gtab[(ix)+ldim*(ldim*(iz+F)+(iy-F))];
-			}
-			if ((sz >= BLOCKZ) && (sy < 2*F) && (sx >= BLOCKX)) {
-				shm[sz+F][sy-F][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz+F)+(iy-F))];			
-			}
-			if ((sz >= BLOCKZ) && (sx < 2*F)) {
-				shm[sz+F][sy][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz+F)+(iy))];
-			}
-			if (sz >= BLOCKZ) {
-				shm[sz+F][sy][sx] = gtab[(ix)+ldim*(ldim*(iz+F)+(iy))];			
-			}
-			if ((sz >= BLOCKZ) && (sx >= BLOCKX)) {
-				shm[sz+F][sy][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz+F)+(iy))];			
-			}
-			if ((sz >= BLOCKZ) && (sy >= BLOCKY) && (sx < 2*F)) {
-				shm[sz+F][sy+F][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz+F)+(iy+F))];			
-			}
-			if ((sz >= BLOCKZ) && (sy >= BLOCKY)) {
-				shm[sz+F][sy+F][sx] = gtab[(ix)+ldim*(ldim*(iz+F)+(iy+F))];			
-			}
-			if ((sz >= BLOCKZ) && (sy >= BLOCKY) && (sx >= BLOCKX)) {
-				shm[sz+F][sy+F][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz+F)+(iy+F))];			
-			}
-			// right:
-			if ((sy < 2*F) && (sx >= BLOCKX)) {
-				shm[sz][sy-F][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz)+(iy-F))];			
-			}
-			if (sx >= BLOCKX) {
-				shm[sz][sy][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz)+(iy))];
-			}	
-			if ((sy >= BLOCKY) && (sx >= BLOCKX)) {
-				shm[sz][sy+F][sx+F] = gtab[(ix+F)+ldim*(ldim*(iz)+(iy+F))];			
-			}
-			// left:
-			if ((sy < 2*F) && (sx < 2*F)) {
-				shm[sz][sy-F][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz)+(iy-F))];
-			}
-			if (sx < 2*F) {
-				shm[sz][sy][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz)+(iy))];
-			}
-			if ((sy >= BLOCKY) && (sx < 2*F)) {
-				shm[sz][sy+F][sx-F] = gtab[(ix-F)+ldim*(ldim*(iz)+(iy+F))];
-			}
-			// top:
-			if (sy < 2*F) {
-				shm[sz][sy-F][sx] = gtab[(ix)+ldim*(ldim*(iz)+(iy-F))];
-			}
-			// bottom:
-			if (sy >= BLOCKY) {
-				shm[sz][sy+F][sx] = gtab[(ix)+ldim*(ldim*(iz)+(iy+F))];
-			}
-			__syncthreads();
-
-			DTYPE v = 0;
-			// apply the filter on each point, average (2*F+1)^3:
-			for (int lz=-F; lz<=F; lz++) {	
-				for (int ly=-F; ly<=F; ly++) {
-					for (int lx=-F; lx<=F; lx++) {
-						v += shm[sz+lz][sy+ly][sx+lx];
-					}
-				}
-			}
-			// save the result on the output tab:
-			gres[idx] = v/((2*F+1)*(2*F+1)*(2*F+1));
-			idx      += pln*BLOCKZ;
 		}
 	}
 }
@@ -348,16 +217,16 @@ int main() {
 	// apply the 3D filter on the cube (CPU):
 	_t = clock();
 	median_filter_cpu(tab, res, ldim);
-	time_used = (clock() - _t); ///CLOCKS_PER_SEC;
+	time_used = (clock() - _t); // /CLOCKS_PER_SEC;
 	// show timing:
 	fprintf(stdout, "median_filter_cpu time      : %10.2f clocks\n", time_used);
 	// apply the 3D filter on the cube (GPU):
 	dim3 threads(BLOCKX, BLOCKY, 1);
 	dim3 blocks((N+BLOCKX-1)/BLOCKX, (N+BLOCKY-1)/BLOCKY, 1);
 	_t = clock();
-	median_filter_gpu<<<blocks,threads>>>(gtab, gres, ldim);
+	median_filter_gpu_gm<<<blocks,threads>>>(gtab, gres, ldim);
 	CUCHK(cudaDeviceSynchronize());
-	time_used = (clock() - _t); ///CLOCKS_PER_SEC;
+	time_used = (clock() - _t); // /CLOCKS_PER_SEC;
 	// show timing:
 	fprintf(stdout, "median_filter_gpu time      : %10.2f clocks ", time_used);
 	// check the results:
@@ -382,10 +251,11 @@ int main() {
 						"\33[1;31mFAILURE(rate: %5.2f%%)\33[m\n", 
 						100*((double)errors/(double)size));
 	else fprintf(stdout, "\33[1;32mSUCCESS\33[m\n");
+	/*
 	// apply the 3D filter on the cube (GPU/3D_sm):
 	CUCHK(cudaMemset((void*)gres, 0, sizeof(DTYPE)*size));
 	dim3 threads_3d(BLOCKX, BLOCKY, BLOCKZ);
-	dim3  blocks_3d((N+BLOCKX-1)/BLOCKX, (N+BLOCKY-1)/BLOCKY, 1);
+	dim3  blocks_3d((N+(2*F)+BLOCKX-1)/BLOCKX, (N+(2*F)+BLOCKY-1)/BLOCKY, 1);
 	_t = clock();
 	median_filter_gpu_sm_3d<<<blocks_3d, threads_3d>>>(gtab, gres, ldim);
 	CUCHK(cudaDeviceSynchronize());
@@ -398,6 +268,7 @@ int main() {
 						"\33[1;31mFAILURE(rate: %5.2f%%)\33[m\n", 
 						100*((double)errors/(double)size));
 	else fprintf(stdout, "\33[1;32mSUCCESS\33[m\n");
+	*/
 	// free the memory:
 	free(tab);
 	free(res);
